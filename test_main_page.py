28ed2630546a26890f7c628695d2ebe5555360b0
gubinvl <gubin.vl@gmail.com>
2019-08-01 00:43:03 +0300

    add need_review mark to tests in test_product_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -10,26 +10,16 @@ class TestLoginFromMainPage(object):
     def setup(self, browser):
         self.link = 'http://selenium1py.pythonanywhere.com'
         self.login_link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
-        self.page = MainPage(browser, self.link)
-        self.page.open()
-        yield
-        del self.page
+        self.init_page = MainPage(browser, self.link)
+        self.init_page.open()
                      
     def test_guest_can_go_to_login_page(self, browser):
-        self.page.go_to_login_page()
+        self.init_page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
-
-    def test_user_can_to_register(self, browser):
-        self.page.go_to_login_page()
-        login_page = LoginPage(browser, browser.current_url)
-        login_page.should_be_login_page()
-        login_page.register_new_user('gubin@gmail.com', 'gowgow321')
-        time.sleep(30)
-        login_page.should_be_authorized_user()
 
     def test_guest_should_see_login_link(self, browser):
-        self.page.should_be_login_link()
+        self.init_page.should_be_login_link()
 
 @pytest.mark.cart_guest
 class TestCartFromMainPage(object):
@@ -38,13 +28,11 @@ class TestCartFromMainPage(object):
     def setup(self, browser):
         self.link = 'http://selenium1py.pythonanywhere.com'
         self.login_link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
-        self.page = MainPage(browser, self.link)
-        self.page.open()
-        yield
-        del self.page
+        self.init_page = MainPage(browser, self.link)
+        self.init_page.open()
 
     def test_guest_cant_see_product_in_cart_opened_from_main_page(self, browser):
-        self.page.go_to_cart_page()
+        self.init_page.go_to_cart_page()
         cart_page = CartPage(browser, browser.current_url)
         cart_page.should_be_empty()
         cart_page.should_be_empty_message()
--- test_product_page.py
+++ test_product_page.py
@@ -9,42 +9,63 @@ class TestLoginFromProductPage(object):
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
         self.link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-        self.page = ProductPage(browser, self.link)
-        self.page.open()
+        self.init_page = ProductPage(browser, self.link)
+        self.init_page.open()
         yield
-        del self.page
 
     def test_guest_should_see_login_link_on_product_page(self):
-        self.page.should_be_login_link()
+        self.init_page.should_be_login_link()
 
+    @pytest.mark.need_review
     def test_guest_can_go_to_login_page_from_product_page(self):
-        self.page.go_to_login_page()
+        self.init_page.go_to_login_page()
 
-@pytest.mark.cart
+@pytest.mark.user_cart
 class TestUserAddToCartFromProductPage(object):
 
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
         self.link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
-        page = ProductPage(browser, self.link)
-        page.open()
-        page.go_to_login_page()
+        self.init_page = ProductPage(browser, self.link)
+        self.init_page.open()
+        self.init_page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
         login_page.register_new_user(faker.Faker().email(), 'gowgow321')
         login_page.should_be_authorized_user()
-        
-    def test_user_can_add_product_to_cart(self, browser):
-        page = ProductPage(browser, self.link)
-        page.open()
-        page.add_product_to_basket()
-        page.solve_quiz_and_get_code()
-        page.should_be_equal_names()
-        page.should_be_equal_prices()
+        self.init_page.open()
 
-    def test_user_cant_see_success_message(self, browser):
-        page = ProductPage(browser, self.link)
-        page.open()
-        page.go_to_cart_page()
+    @pytest.mark.need_review    
+    def test_user_can_add_product_to_cart(self, browser):
+        self.init_page.add_product_to_basket()
+        self.init_page.solve_quiz_and_get_code()
+        self.init_page.should_be_equal_names()
+        self.init_page.should_be_equal_prices()
+
+    def test_user_cant_see_product_in_cart_opened_from_product_page(self, browser):
+        self.init_page.go_to_cart_page()
+        cart_page = CartPage(browser, browser.current_url)
+        cart_page.should_be_empty()
+        cart_page.should_be_empty_message()
+
+@pytest.mark.guest_cart
+class TestGuestAddToCartFromProductPage(object):
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        self.link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+        self.init_page = ProductPage(browser, self.link)
+        self.init_page.open()
+
+    @pytest.mark.need_review    
+    def test_guest_can_add_product_to_cart(self, browser):
+        self.init_page.add_product_to_basket()
+        self.init_page.solve_quiz_and_get_code()
+        self.init_page.should_be_equal_names()
+        self.init_page.should_be_equal_prices()
+
+    @pytest.mark.need_review
+    def test_guest_cant_see_product_in_cart_opened_from_product_page(self, browser):
+        self.init_page.go_to_cart_page()
         cart_page = CartPage(browser, browser.current_url)
         cart_page.should_be_empty()
         cart_page.should_be_empty_message()
